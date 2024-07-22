809057eb5c6834d97086da009f1f6e7b7363485c
Ashik765 <mhdashik765@gmail.com>
2024-07-22 17:08:30 +0530

    broo tkake a look i think we done but not done hahaha i lot to add  , i m too tired  so iam stoping

--- project_demo.py
+++ project_demo.py
@@ -37,7 +37,7 @@ def register(users):                                         # registering
 
     while True:                                                                # this part  from google it help to find invalid gmails
         email = input("Enter your Gmail address: ").strip()
-        if email.endswith('@gmail.com'):
+        if email.endswith('@gmail.com'):           # find the email is valid or invalid
             break
         else:
             print("Invalid email. Please enter a valid Gmail address (ending with @gmail.com).")
@@ -46,17 +46,23 @@ def register(users):                                         # registering
     users[username] = {'password': password, 'email': email}
     print("Registration successful. You can now login.")
 
+    return
+
 # Function to login a user
 def login(users):                                                       # helps user to login
     username = input("Enter your username: ").strip()
     password = input("Enter your password: ").strip()
 
-    if username in users and users[username]['password'] == password:
+    if username in users and users[username]['password'] == password:                # checking the given user nameand pass is in the list or not
         print("Login successful.")
-        return True
+        return username
     else:
         print("Invalid username or password. Try again.")
         return False
+
+
+
+
 
 # Function to load movies from the file
 def load_movies():
@@ -88,43 +94,56 @@ def select_movie():
     for movie in movies:
         print(movie.split('-')[0].strip())  # Display only movie names
 
+# Function to generate available seats with random alphabets
+def generate_seats(num_seats):
+    seats = []
+    alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for i in range(1, 26):
+        seat = f"{random.choice(alphabets)}-{i}"  # choose an alphabet from the variable and add this with seat number
+        seats.append(seat)
+    return seats
+
 # Function to book a movie
-def book_movie():                                     # user can boook movie , seats  , time here
+def book_movie(username):
     view_movies()
     movie_name = input("What movie do you want to watch: ").strip().title()
+
     num_seats = int(input("Enter the number of seats you want to book: ").strip())
-    num_adults = int(input("How many adult tickets (enter 0 if no adult): ").strip() or '0')
-    num_seniors = int(input("How many senior tickets (enter 0 if no senior): ").strip() or '0')
-    num_children = int(input("How many child tickets (enter 0 if no child): ").strip() or '0')
+    num_adults = int(input("How many adult tickets RM 20 (enter 0 if no adult): ").strip() or '0')
+    num_seniors = int(input("How many senior tickets RM 15 (enter 0 if no senior): ").strip() or '0')
+    num_children = int(input("How many child tickets RM 10 (enter 0 if no child): ").strip() or '0')
     show_time = input("Enter the show time (choose from list): ").strip()
 
     total_tickets = num_adults + num_seniors + num_children
 
+    seats = generate_seats(num_seats)
+
     if total_tickets > num_seats:                                 # if user input more seats
         print("Not enough seats available. Try booking fewer tickets.")
-        return 0
+        return 0, [], 0, 0, 0
 
-    available_seats = random.sample(range(1, 25), total_tickets)  # Randomly selecting seats
+    available_seats = random.sample(seats, total_tickets)  # Randomly selecting seats
 
-    cost = (num_adults * 20) + (num_seniors * 15) + (num_children * 10)
+    cost = (num_adults * 20) + (num_seniors * 15) + (num_children * 10)            #calcualating the price for movie
     print(" ")
-    print(f"Total cost: RM{cost:>15}")
+    print(f"Total cost: RM{cost:.2f}")
 
-    with open('bookinginfo.txt', 'a') as file:
-        file.write(f"{movie_name}|{show_time}|{available_seats}|{cost}\n")
+    with open('bookinginfo.txt', 'a') as file:               # saving the booking to booking info text file
+        file.write(f"{username}|{movie_name}|{show_time}|{available_seats}|{cost}\n")
 
     print("Booking successful!")
-    return cost
+  # returning  variables
+    return cost, available_seats, num_children, num_seniors, num_adults
 
 # foood and beverage part :
 
-def load_combos():
+def load_combos():    # loading the combo food and beverage from text file
     combos = []
     with open('combo.txt', 'r') as file:
         for line in file:
-            combo_id, combo_name, price = line.strip().split('-')
+            combo_id, combo_name, price = line.strip().split('-')     # Split line into combo ID, name, and price
             price = price.replace('RM', '').strip()  # Remove 'RM' prefix
-            combos.append({'id': combo_id.strip(), 'name': combo_name.strip(), 'price': float(price)})
+            combos.append({'id': combo_id.strip(), 'name': combo_name.strip(), 'price': float(price)})    # add combo to list
     return combos
 
 def load_snacks():
@@ -136,7 +155,7 @@ def load_snacks():
             snacks.append({'name': snack_name.strip(), 'price': float(price)})
     return snacks
 
-def load_drinks():
+def load_drinks():                    #laoding
     drinks = []
     with open('drinks.txt', 'r') as file:
         for line in file:
@@ -195,8 +214,8 @@ def add_food():
             snack_choice = int(input("Select snacks by number: ").strip())
             snacks = load_snacks()
             if 1 <= snack_choice <= len(snacks):
-                qty = int(input("Enter the quantity: "))
-                order.append((snacks[snack_choice - 1], qty))
+                qty = int(input("Enter the quantity: "))   # Get quantity of selected snack
+                order.append((snacks[snack_choice - 1], qty))   # Add selected snack to order
                 print(f"Added {qty} x {snacks[snack_choice - 1]['name']} to your order.")
             else:
                 print("Invalid selection")
@@ -207,7 +226,7 @@ def add_food():
             drinks = load_drinks()
             if 1 <= drink_choice <= len(drinks):
                 qty = int(input("Enter the quantity: "))
-                order.append((drinks[drink_choice - 1], qty))
+                order.append((drinks[drink_choice - 1], qty))  # Add selected drink to order
                 print(f"Added {qty} x {drinks[drink_choice - 1]['name']} to your order.")
             else:
                 print("Invalid selection")
@@ -219,19 +238,22 @@ def add_food():
 
     return order
 
-# Calculating the payment
+# Calculate the total cost of the order
 def calculate_total(order):
     total = 0
     for item, quantity in order:
-        total += item['price'] * quantity
+        total += item['price'] * quantity      # Sum up total cost
     return total
 
 def payment_process(movie_cost, food_order):
     if movie_cost is None:
         movie_cost = 0
 
-    food_total = calculate_total(food_order)
-    total_amount = food_total + movie_cost
+    food_total = calculate_total(food_order)    # calculating food cost
+    total_amount = food_total + movie_cost      # total amount including the movie cost
+
+
+     # printing the paymnet for double check
 
     print('')
     print("Calculating the Total:")
@@ -244,41 +266,56 @@ def payment_process(movie_cost, food_order):
     total_payment = 0
 
     while total_payment < total_amount:
-        amount = float(input("Enter the paymnet amount : ").strip())
+        amount = float(input("Enter the paymnet amount(only accept 20,50,100 notes): ").strip())
         total_payment += amount
 
         if total_payment <total_amount:
-            print(f"insufficient amount . you need to enter RM {total_amount - total_payment:.2f} more")
+            print(f"insufficient amount . you need to enter RM {total_amount - total_payment:.2f} more")    # Notify if more money is needed
         else:
             break
 
-    change = total_payment - total_amount
+    change = total_payment - total_amount        # calculate the change
     print(f"Payment successful. Your change is RM {change:.2f}.")
 
     return total_amount, total_payment, change
 
-def print_receipt(movie_cost, food_order, total_amount, payment_amount, change):                # printing reciept
-    print("---------------------------------------------------------")
+
+#printing reciept
+
+def print_receipt(movie_cost, food_order, total_amount, payment_amount, change, available_seats , num_children , num_seniors , num_adults ):
+    print("\n---------------------------------------------------------")
     print("\t\t\tMarwan Movie Ticket Booking System")
     print("---------------------------------------------------------")
-    print(f"Date: {current_date:<15} Time: {current_time:<15}")
+    print(f"Date: {current_date:<15} Time: {current_time:<35}")
     print('---------------------------------------------------------')
-    print(f"ITEMS                         {'COST':>10}")
+    print(f"ITEMS                         {'COST':>7.5}")
     print(f"Movie cost                   : RM {movie_cost:>5.2f}")
+    # Display selected seats
+    print("Selected seats:")
+    for seat in available_seats:
+        print(f"  {seat}")  # addding seats detail to reciept
+    print(f"Adults: {num_adults}, Seniors: {num_seniors}, Children: {num_children}")
     print(f"Food and Beverages Cost      : RM {calculate_total(food_order):<15.2f}")
     print("----------------------------------------------------------")
+
     print(f"Total Cost                   : RM {total_amount:<15.2f}")
     print(f"Amount Paid                  : RM {payment_amount:<15.2f}")
     print("----------------------------------------------------------")
     print(f"Change                       : RM {change:<15.2f}")
-    print("\t\t\tThank From Marwan booking System ")
-    print("\t\t\tave a great day!")
     print('')
+    print("\t\t\tThank you from Marwan Booking System")
+    print("\t\t\t\t\thave a great day!")
+    print('')
+
 
 # Main function to integrate the add_food function into the main workflow
 def main():
+    #load user from text file
+
     users = load_users()
     print("Welcome to the Movie Ticket Booking System!")
+    username = []
+
 
     while True:
         print("1. Register")
@@ -287,14 +324,18 @@ def main():
         if choice == 1:
             register(users)
         elif choice == 2:
-            if login(users):
-                break
+            username = login(users)
+            if username:
+                break            # Login user and break the loop if successful
         else:
             print("Invalid choice. Please enter '1' for Register or '2' for Login.")
 
     movie_cost = 0
+    available_seats = []                              # Initialize available seats
+    num_children = num_seniors = num_adults = 0          # Initialize ticket counts
+
     print("-------------------------------------")
-    print("\nBook your movie here")
+    print("Book your movie here")
     print("------------------------------------")
     while True:
         print("\n1. Show movie details")
@@ -303,16 +344,16 @@ def main():
         option = input("Enter your choice (type 'back' to exit): ").strip().lower()
 
         if option == '1':
-            view_movies()
+            view_movies()  #show movie details
         elif option == '2':
-            movie_cost = book_movie()
+            movie_cost, available_seats, num_children, num_seniors, num_adults = book_movie(username)        # Book a movie and get details
         elif option == '3':
-            food_order = add_food()
-            total_amount, payment_amount, change = payment_process(movie_cost, food_order)
-            print_receipt(movie_cost, food_order, total_amount, payment_amount, change)
+            food_order = add_food()   # adding food and beverages
+            total_amount, payment_amount, change = payment_process(movie_cost, food_order)   # for payment process
+            print_receipt(movie_cost, food_order, total_amount, payment_amount, change, available_seats, num_children, num_seniors, num_adults)  #print reciept
             break  # Exit the loop after payment and receipt
         else:
-            print("Invalid choice. Please enter '1', '2', or '3'.")
+            print("Invalid choice. Please enter '1', '2', or '3'.")    # invalid choice
 
 if __name__ == "__main__":
     main()
